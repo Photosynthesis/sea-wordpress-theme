7cd37a555f15c45a1e8e269bf69f8e383ddf8c70
AM <adam@photosynth.ca>
2021-03-10 15:04:40 +0100

    Removed fullscreen iframe code

--- inc/blocks/map.php
+++ inc/blocks/map.php
@@ -55,76 +55,16 @@ if (($logo = get_field('logo'))) {
 
 $map_url = get_field('map_url') . '?' . http_build_query($attributes, '', '&');
 
+$map_style = "width:100%; height: ".(get_field('height') ? : '500') . 'px;';
+
 ?>
 <div id="sea-iframe-container">
   <div id="sea-iframe-box" class="iframe-box-normal">
-    <iframe id="sea-map-iframe" src="<?= $map_url ?>"></iframe>
+    <iframe id="sea-map-iframe" src="<?= $map_url ?>" style="<?= $map_style ?>"></iframe>
     <div class="cover"></div>
-
-    <span id="sea-map-fullscreen-control" class="dashicons dashicons-fullscreen-alt" ></span>
   </div>
 </div>
 
 <div class="wp-block-buttons">
   <div class="wp-block-button"><a class="wp-block-button__link" href="<?= $map_url ?>" target="_blank">View Map In Full-screen</a></div>
 </div>
-
-<style type="text/css">
-
-#sea-iframe-container{
-  height: <?= (get_field('height') ?: '500') . 'px'; ?>;
-  width:100%;
-}
-
-#sea-iframe-box{
-  width: 100%;
-  height: 100%;
-}
-
-#sea-map-iframe{
-  width: 100%;
-  height: 100%;
-}
-
-.iframe-box-fullscreen{
-  position: absolute;
-  /*top: 0;*/
-  left: 0;
-  z-index: 1000;
-}
-
-.iframe-box-normal{
-  position: relative;
-}
-
-#sea-map-fullscreen-control{
-  cursor: pointer;
-  position: absolute;
-  top: 10px;
-  right: 40px;
-  font-size:50px;
-  opacity: 0.9;
-}
-
-</style>
-
-<script type="text/javascript">
-
-var iframeControl = document.getElementById('sea-map-fullscreen-control');
-
-iframeControl.onclick = function(){
-  var iframeBox = document.getElementById('sea-iframe-box');
-  //iframeBox.requestFullscreen();
-  if(iframeBox.className == "iframe-box-normal"){
-    iframeBox.className = "iframe-box-fullscreen";
-    iframeControl.className = "dashicons dashicons-fullscreen-exit-alt";
-    document.body.style.overflow = "hidden";
-    iframeBox.style.top = window.pageYOffset+"px";
-  }else{
-    iframeBox.className = "iframe-box-normal";
-    iframeControl.className = "dashicons dashicons-fullscreen-alt";
-    document.body.style.overflow = "auto";
-    iframeBox.style.top = "";
-  }
-}
-</script>
